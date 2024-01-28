a8080480a0619575ce9ceb0c58dd4b71e10cd43f
Sebastian Castillo <tantrum5535@gmail.com>
2024-01-28 11:49:48 -0300

    Actualizando el proyecto base

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
-    eventListeners()
+document.addEventListener('DOMContentLoaded', function() {
+
+    eventListeners();
 
     darkMode();
-})
+});
 
 function darkMode() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // console.log(prefiereDarkMode.matches);
+
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
-    botonDarkMode.addEventListener('click', ()=>{
+    botonDarkMode.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
-        botonDarkMode.classList.toggle('active');
-    })
+    });
 }
 
 function eventListeners() {
-    const mobileMenu = document.querySelector('.mobile-menu')
-    mobileMenu.addEventListener('click', navegacionResponsive)
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click', navegacionResponsive);
 }
 
 function navegacionResponsive() {
-    const navegacion = document.querySelector('.navegacion')
+    const navegacion = document.querySelector('.navegacion');
 
-    if (navegacion.classList.contains('mostrar')) {
-        navegacion.classList.remove('mostrar')
-    } else {
-        navegacion.classList.add('mostrar')
-    }
-
-    // tambien se puede hacer con un toggle
-    // navegacion.classList.toggle('mostrar')
-
+    navegacion.classList.toggle('mostrar')
 }
