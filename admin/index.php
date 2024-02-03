3bc814752d4a4e76b6bba7c98cfb79ebe287d0e0
Sebastian Castillo <tantrum5535@gmail.com>
2024-02-03 18:03:53 -0300

    creando una tabla para mostrar los datos de la bbdd

--- admin/index.php
+++ admin/index.php
@@ -12,6 +12,29 @@ incluirTemplate('header');
         <p class="alerta exito">Anuncio creado correctamente</p>
     <?php endif; ?>
     <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+
+    <table class="propiedades">
+        <thead>
+            <th>ID</th>
+            <th>Titulo</th>
+            <th>Imagen</th>
+            <th>Precio</th>
+            <th>Acciones</th>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Casa en la playa</td>
+                <td><img src="/imagenes/85bf4c3057dcae5ddbd646c4293fcc9a.jpg" alt="imagen" class="imagen-tabla"></td>
+                <td>$1200000</td>
+                <td>
+                    <a href="#" class="boton-rojo-block">Eliminar</a>
+                    <a href="#" class="boton-amarillo-block">Actualizar</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </main>
 
 <?php
